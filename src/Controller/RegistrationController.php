acef43dadec02937d6cdb8f6d50b8d7f4d0b5875
Argtum <Silwar40k@gmail.com>
2019-01-04 02:14:12 +0300

    Сделал регистрацию пользователей

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,19 +15,27 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/registration", name="registration")
      * @param EntityManagerInterface $em
+     * @param Request $request
      * @return Response
+     * @throws \Exception
      */
-    public function newUser(EntityManagerInterface $em)
+    public function newUser(EntityManagerInterface $em, Request $request)
     {
         $form = $this->createForm(RegistrationFormType::class);
 
-//        $user = new User();
-//        $user->setNickname('Silver')
-//             ->setPassword('password')
-//             ->setRegistrationDate(\DateTime::createFromFormat('Y-m-d', '2019-01-02'));
-//
-//        $em->persist($user);
-//        $em->flush();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $user = new User();
+            $user->setUsername($data['username'])
+                 ->setPassword($data['password'])
+                 ->setRegistrationDate(new \DateTime());
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('playground');
+        }
 
         return $this->render('registration/index.html.twig', [
             'registrationForm' => $form->createView()
